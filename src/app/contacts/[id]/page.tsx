8d8b00e7f5f3679f1e26cba7b284c03c1daf1dde
Jonathan Gaze <jongaze@gmail.com>
2025-05-29 15:00:19 +0100

    TS Issues fixed 4

--- src/app/contacts/[id]/page.tsx
+++ src/app/contacts/[id]/page.tsx
@@ -1,13 +1,15 @@
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import ContactDetailsClient from './client';
+import React from 'react';
 
-export default async function ContactPage({ params }: { params: { id: string } }) {
+export default async function ContactPage({ params }: { params: Promise<{ id: string }> }) {
   const session = await getServerSession();
+  const { id } = await params;
   
   if (!session) {
     redirect('/login');
   }
 
-  return <ContactDetailsClient contactId={params.id} />;
+  return <ContactDetailsClient contactId={id} />;
 } 
